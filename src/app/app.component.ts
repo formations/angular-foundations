dab1d6f4215dcbef9cea071e6f1cd58308f45b3f
Nicolas Fränkel <nicolas@frankel.ch>
2018-07-01 14:19:24 +0200

    Prepare the app component to start the workshop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  id = 21464;
+  title = 'Powers (2000)';
+  issueNumber = 18;
+  thumbnail = 'image/image_not_available.jpg';
+  price = 3.45;
+  description = 'Walker and Pilgrim investigate the death of a member of FG-3, a federally employed and corporate funded trio. Evidence leads Walker to believe a federal cover-up is taking place, and as the investigation spirals out of control both professionally and personally (Zora is killed during a confrontation with FG-3), Walker leaves the police force after exposing the cover-up on television.';
 }
