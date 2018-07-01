5552d9a0e2888622c7ea56d09a90de5c4b0a678d
Nicolas Fränkel <nicolas@frankel.ch>
2018-07-01 15:49:02 +0200

    Make use of existing class

--- src/app/comics-list/comics-list.component.ts
+++ src/app/comics-list/comics-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SimpleComic } from '../model';
 
 @Component({
   selector: 'app-comics-list',
@@ -7,10 +8,12 @@ import { Component } from '@angular/core';
 })
 export class ComicsListComponent {
 
-  id = 21464;
-  title = 'Powers (2000)';
-  issueNumber = 18;
-  thumbnail = 'image/image_not_available.jpg';
-  price = 3.45;
-  description = 'Walker and Pilgrim investigate the death of a member of FG-3, a federally employed and corporate funded trio. Evidence leads Walker to believe a federal cover-up is taking place, and as the investigation spirals out of control both professionally and personally (Zora is killed during a confrontation with FG-3), Walker leaves the police force after exposing the cover-up on television.';
+  comic: SimpleComic = {
+    id: 21464,
+    title: 'Powers (2000)',
+    issueNumber: 18,
+    thumbnail: 'image/image_not_available.jpg',
+    price: 3.45,
+    description: 'Walker and Pilgrim investigate the death of a member of FG-3, a federally employed and corporate funded trio. Evidence leads Walker to believe a federal cover-up is taking place, and as the investigation spirals out of control both professionally and personally (Zora is killed during a confrontation with FG-3), Walker leaves the police force after exposing the cover-up on television.'
+  }
 }
