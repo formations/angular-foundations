1f23865f0652ec24dd7b1f4e98ead59aae668e69
Nicolas Fränkel <nicolas@frankel.ch>
2018-07-01 18:09:11 +0200

    Make use of existing data

--- src/app/comics-list/comics-list.component.ts
+++ src/app/comics-list/comics-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SimpleComic } from '../model';
+import { COMIC } from '../data';
 
 @Component({
   selector: 'app-comics-list',
@@ -8,12 +9,5 @@ import { SimpleComic } from '../model';
 })
 export class ComicsListComponent {
 
-  comic: SimpleComic = {
-    id: 21464,
-    title: 'Powers (2000)',
-    issueNumber: 18,
-    thumbnail: 'image/image_not_available.jpg',
-    price: 3.45,
-    description: 'Walker and Pilgrim investigate the death of a member of FG-3, a federally employed and corporate funded trio. Evidence leads Walker to believe a federal cover-up is taking place, and as the investigation spirals out of control both professionally and personally (Zora is killed during a confrontation with FG-3), Walker leaves the police force after exposing the cover-up on television.'
-  }
+  comic: SimpleComic = COMIC;
 }
