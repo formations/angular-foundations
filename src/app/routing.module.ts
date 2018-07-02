a593883788f62ea67e070273d00f8ed62b473004
Nicolas Fränkel <nicolas@frankel.ch>
2018-07-02 17:26:02 +0200

    Implement routes

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ComicsListComponent } from './comics-list/comics-list.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'comics', component: ComicsListComponent }
+];
 
 @NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
 export class RoutingModule { }
