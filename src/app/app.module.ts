4d3bc0ffbd713eefc2f4cc6638996be19fad9d71
Nicolas Fränkel <nicolas@frankel.ch>
2018-07-02 11:40:44 +0200

    Prepare Navigation Bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { TabBarComponent } from './tab-bar/tab-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    TabBarComponent
+    TabBarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
