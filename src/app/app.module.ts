6d2caf74e381f57784887a5e7e555533ddfa34fe
Nicolas Fränkel <nicolas@frankel.ch>
2018-07-06 10:32:46 +0200

    Prepare Tab Bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { TabBarComponent } from './tab-bar/tab-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    TabBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tab-bar/tab-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-bar',
+  templateUrl: './tab-bar.component.html',
+  styleUrls: ['./tab-bar.component.css']
+})
+export class TabBarComponent { }
